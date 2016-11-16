23b55035877a0ab41c771f7a6b5d662f069aa5b3
Georgi Alexandrov <georgi.alexandrov@telerik.com>
2016-11-16 13:58:08 +0200

    Ensure Node 6 is used

--- step.sh
+++ step.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+echo "Ensure Node 6 LTS"
+
+brew uninstall node
+brew tap homebrew/versions
+brew install homebrew/versions/node6-lts
+node -v
+
 VERSION=""
 
 echo "Available NativeScript versions: $(npm view nativescript versions)"
@@ -10,5 +17,12 @@ if [ ! -z "${version}" ] ; then
     VERSION="@${version}"
 fi
 
-echo '' | npm install -g ${npm_options} nativescript${VERSION}
-echo '' | tns doctor
+echo "n
+y
+y
+q
+agree
+N
+n
+n" | npm install -g ${npm_options} nativescript${VERSION}
+echo n | tns doctor
